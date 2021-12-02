be1991e578f3a93e35a2792ede5db53b4bba7800
Syahir <67471898+Syahirdev@users.noreply.github.com>
2021-12-02 14:42:35 +0800

    add topbar component

--- src/components/topbar/Topbar.tsx
+++ src/components/topbar/Topbar.tsx
@@ -1,9 +1,19 @@
 import "./Topbar.scss";
+import { Search } from "@mui/icons-material";
 
 export const Topbar = () => {
     return (
         <div className="topbar">
-            topbar
+            <div className="topbarLeft">
+                <img src="./assets/logo.png" alt="logo" className="topbarLogo"/>
+                <div className="topbarText">Discovery</div>
+                <div className="topbarText">Channel</div>
+            </div>
+            <div className="topbarRight">
+                <div className="topbarText highlight">Signup</div>
+                <div className="topbarText topbarButton">Login</div>
+                <Search className='topbarIcon'/>
+            </div>
         </div>
     );
 };
